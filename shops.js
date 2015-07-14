a694b532dc26cc2c6438a027ad08025c9f6fdc99
Chris Holt <christopher.d.holt@gmail.com>
2015-07-14 15:04:17 -0700

    header auto creates in function, removed space, cleaned up redundancies, put hourly calculation into daily calculation.

--- shops.js
+++ shops.js
@@ -1,5 +1,4 @@
-//Top Pot Domain Model
-
+'use strict'
 var Shop = function(shopName, avgNum, minCust, maxCust, hoursOpen) {
   this.shopName = shopName;
   this.avgNum = avgNum;
@@ -7,40 +6,46 @@ var Shop = function(shopName, avgNum, minCust, maxCust, hoursOpen) {
   this.maxCust = maxCust;
   this.hoursOpen = hoursOpen;
   this.hourly = [];
+  this.totalPerDay = 0
   this.randCust = function (){
   return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
   }
 };
 
-//generates random number of donuts per hour by calling random custers and multiplying it by average donuts/customer.
-Shop.prototype.donutsPerHour = function() {
-  return Math.floor(this.randCust() * this.avgNum);
-};
-
-//Runs loop to calculate number donuts per day based on the random donutsPerHour. Could also use: this.hourly[i] = (this.donutsPerHour());
-Shop.prototype.donutsPerDay = function() {
-  var totalPerDay = 0;
-
-  for (var i = 0; i < this.hoursOpen; i++) {
-    totalPerDay += this.donutsPerHour();
-    this.hourly.push(this.donutsPerHour());
-  }
-  return totalPerDay;
-};
-
 Shop.prototype.render = function() {
   this.donutsPerDay();
   var addRow = document.createElement("tr");
-  var tableSet = document.getElementById("table");
   var rowData = "<td><strong>" + this.shopName + "</strong></td>";
-
   for (var j=0; j < this.hourly.length; j++) {
     rowData = rowData + "<td>" + this.hourly[j] + "</td>";
     console.log(rowData);
   }
-  addRow.innerHTML = rowData + "<td>" + this.donutsPerDay() + "</td>";
+  addRow.innerHTML = rowData + "<td>" + this.totalPerDay + "</td>";
   tableSet.appendChild(addRow);
 };
+
+//Runs loop to calculate number donuts per day based on the random donutsPerHour. Includes total Per Day in Math.random.
+Shop.prototype.donutsPerDay = function() {
+  for (var i = 0; i < this.hoursOpen; i++) {
+    this.totalPerDay += Math.floor(this.randCust() * this.avgNum);
+    this.hourly.push(Math.floor(this.randCust() * this.avgNum));
+  }
+};
+
+var tableSet = document.getElementById("tblBody");
+var addRow = document.createElement("tr");
+
+var header = (function() {
+  var newRow = document.createElement("tr");
+  var hdData = "<td><strong>" + "Locations" + "</strong></td>";
+  for (var h=0; h < 11; h++) {
+    var time = 700 + (h * 100);
+    hdData += "<td><strong>" + time + "</strong></td>";
+  }
+  hdData += "<td><strong>" + "Totals" + "</strong></td>";
+  tableSet.appendChild(newRow);
+  newRow.innerHTML = hdData;
+})();
 
 var downtown = new Shop("Downtown", 4.50, 8, 43, 11);
 var capHill = new Shop("Capitol Hill", 2, 4, 37, 11);
@@ -54,6 +59,4 @@ capHill.render();
 southLake.render();
 wedgewood.render();
 ballard.render();
-
-
 
