4ee17984432c6011dc370902e15b0a3b484627be
Chris Holt <christopher.d.holt@gmail.com>
2015-07-15 11:05:45 -0700

    initial working form w/ event to add

--- shops.js
+++ shops.js
@@ -7,6 +7,7 @@ var Shop = function(shopName, avgNum, minCust, maxCust, hoursOpen) {
   this.hoursOpen = hoursOpen;
   this.hourly = [];
   this.totalPerDay = 0
+  // this.createShop();
   this.randCust = function (){
   return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
   }
@@ -47,6 +48,7 @@ var header = (function() {
   newRow.innerHTML = hdData;
 })();
 
+//
 var downtown = new Shop("Downtown", 4.50, 8, 43, 11);
 var capHill = new Shop("Capitol Hill", 2, 4, 37, 11);
 var southLake = new Shop("South Lake Union", 6.33, 9, 23, 11);
@@ -60,3 +62,20 @@ southLake.render();
 wedgewood.render();
 ballard.render();
 
+
+// Starting with events var newShop grabs the form // newShop.addEventListener("submit" - adds event listener looking for submit on form // the empty function is held inside the event listener, but could be outside. Reasoning Carl put inside was for scope. need to declare all variables for info on form. Then create a new shop (createShop) passing properties into SHOP constructor. Standard practice is adding listener to form when you submit.
+
+var newShop = document.getElementById("newShop");
+newShop.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  var elName = document.getElementById("shopName").value;
+  var elAvgNum = parseInt(document.getElementById("avgNum").value);
+  var elMinCust = parseInt(document.getElementById("minCust").value);
+  var elMaxCust = parseInt(document.getElementById("maxCust").value);
+  var elHours = parseInt(document.getElementById("openHours").value);
+
+
+  var createShop = new Shop(elName, elAvgNum, elMinCust, elMaxCust, elHours);
+  createShop.render();
+});
