2738a42af4f1bbae63f99182a25973a4fb329ca6
Chris Holt <christopher.d.holt@gmail.com>
2015-07-15 12:55:22 -0700

    added id's to render function

--- shops.js
+++ shops.js
@@ -16,9 +16,9 @@ var Shop = function(shopName, avgNum, minCust, maxCust, hoursOpen) {
 Shop.prototype.render = function() {
   this.donutsPerDay();
   var addRow = document.createElement("tr");
-  var rowData = "<td><strong>" + this.shopName + "</strong></td>";
+  var rowData = "<td id=" + "'" + this.shopName + "'><strong>" + this.shopName + "</strong></td>";
   for (var j=0; j < this.hourly.length; j++) {
-    rowData = rowData + "<td>" + this.hourly[j] + "</td>";
+    rowData = rowData + "<td id=" + "'" + this.shopName + "'>" + this.hourly[j] + "</td>";
     console.log(rowData);
   }
   addRow.innerHTML = rowData + "<td>" + this.totalPerDay + "</td>";
@@ -48,20 +48,6 @@ var header = (function() {
   newRow.innerHTML = hdData;
 })();
 
-//
-var downtown = new Shop("Downtown", 4.50, 8, 43, 11);
-var capHill = new Shop("Capitol Hill", 2, 4, 37, 11);
-var southLake = new Shop("South Lake Union", 6.33, 9, 23, 11);
-var wedgewood = new Shop("Wedgewood", 1.25, 2, 28, 11);
-var ballard = new Shop("Ballard", 3.75, 8, 58, 11);
-
-//Call it.
-downtown.render();
-capHill.render();
-southLake.render();
-wedgewood.render();
-ballard.render();
-
 
 // Starting with events var newShop grabs the form // newShop.addEventListener("submit" - adds event listener looking for submit on form // the empty function is held inside the event listener, but could be outside. Reasoning Carl put inside was for scope. need to declare all variables for info on form. Then create a new shop (createShop) passing properties into SHOP constructor. Standard practice is adding listener to form when you submit.
 
@@ -75,7 +61,20 @@ newShop.addEventListener("submit", function(e) {
   var elMaxCust = parseInt(document.getElementById("maxCust").value);
   var elHours = parseInt(document.getElementById("openHours").value);
 
-
   var createShop = new Shop(elName, elAvgNum, elMinCust, elMaxCust, elHours);
   createShop.render();
 });
+
+//
+var downtown = new Shop("Downtown", 4.50, 8, 43, 11);
+var capHill = new Shop("Capitol Hill", 2, 4, 37, 11);
+var southLake = new Shop("South Lake Union", 6.33, 9, 23, 11);
+var wedgewood = new Shop("Wedgewood", 1.25, 2, 28, 11);
+var ballard = new Shop("Ballard", 3.75, 8, 58, 11);
+
+//Call it.
+downtown.render();
+capHill.render();
+southLake.render();
+wedgewood.render();
+ballard.render();
